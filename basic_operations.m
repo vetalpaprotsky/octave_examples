f090c859c4cb90b0e086431780d00d7c60ab2526
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-06-28 10:39:04 +0300

    init

---
+++ basic_operations.m
@@ -0,0 +1,56 @@
+% ; - semicolon at the end of the line suppress output
+% ans - variable which stores last expression return value
+
+3 + 2;
+3 - 2;
+3 / 2;
+3 * 2;
+3 ^ 2;
+
+3 == 2; % equal? (returns false)
+3 ~= 2; % not equal? (returns true)
+bool_value = true;
+
+1 && 0;    % AND
+1 || 0;    % OR
+xor(1, 0); % XOR
+
+PS1('>> '); % change prompt
+
+str = 'hello world';
+disp(str)                              % output variable str
+disp(sprintf('2 decimals: %0.2f', pi)) % pi is a constant
+
+disp(pi)
+format long  % start outputing more digits after the decimal point
+disp(pi)
+format short % go back to the default number of digitis after the decimal point
+disp(pi)
+
+A = [1 2; 3 4; 5 6]; % 3x2 matrix
+B = [                % 2x2 matrix
+    1 2
+    3 4
+];
+disp(A)
+[1 2 3];   % 1x3 matrix(or just row vector)
+[1; 2; 3]; % 3x1 matrix(or just column vector)
+
+1:0.2:2; % row vector with elements starting from 1 to 2 with step 0.2
+1:6;     % [1 2 3 4 5 6]
+
+ones(2, 3);     % 2x3 matrix with all elements equal to 1
+2 * ones(2, 3); % 2x3 matrix with all elments equal to 2
+zeros(2, 3);    % 2x3 matrix with all elments equal to 0
+
+rand(3, 3);  % random 3x3 matrix with numbers between 0 and 1(uniform distribution)
+randn(3, 3); % random 3x3 matrix(normal distribution with mean 0 and standard deviation 1)
+
+% random row vector(normal distribution with mean -6 and standard deviation sqrt(10))
+w = -6 * sqrt(10)*randn(1, 10000);
+% hist(w);     plot a histogram of values in vector w
+% hist(w, 50); histogram with 50 buckets
+
+I = eye(4); % 4x4 identity matrix
+
+% help eye; get help on eye function
---
+++ moving_data_around.m
@@ -0,0 +1,60 @@
+A = [
+    1 2
+    3 4
+    5 6
+];
+size(A);    % rows and columns of the matrix(row vector [3 2])
+size(A, 1); % number of rows
+size(A, 2); % number of columns
+
+v = [1 2 3 4];
+length(v); % size of the longest dimension(it's 4)
+length(A); % 3(because A is 3x2 matrix)
+
+% pwd; % get current directory path
+% cd;  % change directory
+% ls;  % list files
+
+load featuresX.dat;             % load dataset in matrix(it'll be saved in featuresX variable)
+% load('featuresX.dat');        % works the same as load featuresX.dat;
+priceY = load('featuresY.dat'); % featuresY variable won't be created
+
+% who; % show all variables in the current scope(it also returns row vector with variables)
+whos;  % who with more details
+
+clear v; % remove v variable
+% clear; remove all variables
+
+% elements indexing starts from 1
+% priceY is a column vector(10x1 matrix)
+priceY(3);      % get element at row 3
+priceY([1, 4]); % get elements at rows 1 and 4
+priceY(2:6);    % get elements at rows from 1 to 6(2:6 is [2 3 4 5 6])
+% implicitly column 1 is used everywhere in the priceY examples
+
+% featuresX is a 10x2 matrix
+featuresX(3, 2);     % get element at row 3, column 2
+featuresX([1 4], 2); % get elements at rows 1 and 4, column 2
+featuresX([1 4]);    % get elements at rows 1 and 4, column 1
+featuresX(2:5, 1:2); % get elements at rows from 2 to 5, columns from 1 to 2(4x2 matrix)
+featuresX(2, :);     % get all elements from the second row(row vector)
+featuresX(:, 2);     % get all elements from the second column(column vector)
+% general rule for getting submatrices: (rows, columns)
+
+f = featuresX(1:5, 1:2);
+save some_features.mat f; % save matrix f to some_features.mat file(take a look at this file)
+load some_features.mat;   % load some_features.mat to f variable(f is the name of the matrix which is saved in that file)
+
+save some_features_bin.mat f -binary; % save file as binary
+save some_features_text.mat f -ascii; % save file as text(not the same as without -ascii)
+
+B = [
+    10 11;
+    12 13;
+    14 15;
+];
+A(:, 2) = [10; 11; 12];  % assign this column vector to the second column of matrix A
+[A [100; 101; 102]];     % append this column vector to the right of matrix A
+[A B];                   % append matrix B to matrix A from the right
+[A; B];                  % append matirx B to matrix A from the bottom
+A(:);                    % put all elements of A into single column vector
