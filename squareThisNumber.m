94c98b38f31d12c7d44761cbb843a969a222a867
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-06-29 12:08:10 +0300

    add comments

--- squareThisNumber.m
+++ squareThisNumber.m
@@ -1,5 +1,13 @@
 % function definition
 % each function has it's own file
+
+% to call this function just open the folder where this file is using cd
+% commad(in Octave interpreter) and run squareThisNumber(myVar)
 function y = squareThisNumber(x)
 
 y = x^2;
+
+% Octave search path
+% addpath('/full/path/to/my/folder') after adding this path, Octave will be
+% searching for function definition in that folder as well and you won't
+% need to go to a specific folder in order to run a function which is there
